9d37e8616ebe1f80ebf1038477adf97ac50488d4
Burns, Cody <cody.burns@accenture.com>
2020-08-30 16:09:55 -0500

    remove ternary

--- consensus/ethash/ethash.go
+++ consensus/ethash/ethash.go
@@ -565,7 +565,12 @@ func (ethash *Ethash) setDag()  error {
 // by first checking against a list of in-memory caches, then against caches
 // stored on disk, and finally generating one if none can be found.
 func (ethash *Ethash) cache(block uint64) *cache {
-	epoch := ethash.config.ECIP1043 > 0 ? ethash.config.ECIP1043 : block / epochLength
+	if ethash.config.ECIP1043 > 0 {
+		epoch := ethash.config.ECIP1043
+	} else {
+		epoch := block / epochLength
+	}
+		
 	currentI, futureI := ethash.caches.get(epoch)
 	current := currentI.(*cache)
 
@@ -588,7 +593,11 @@ func (ethash *Ethash) cache(block uint64) *cache {
 // generates on a background thread.
 func (ethash *Ethash) dataset(block uint64, async bool) *dataset {
 	// Retrieve the requested ethash dataset
-	epoch := ethash.config.ECIP1043 > 0 ? ethash.config.ECIP1043 : block / epochLength
+	if ethash.config.ECIP1043 > 0 {
+		epoch := ethash.config.ECIP1043
+	} else {
+		epoch := block / epochLength
+	}
 	currentI, futureI := ethash.datasets.get(epoch)
 	current := currentI.(*dataset)
 
