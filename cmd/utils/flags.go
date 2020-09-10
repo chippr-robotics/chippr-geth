eea346b9f2adb9f8fd98fe08d0001453670f3797
dontpanic <cody.w.burns@gmail.com>
2020-09-10 23:19:31 +0000

    yolo mode enabled

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -219,7 +219,7 @@ var (
 	defaultSyncMode = eth.DefaultConfig.SyncMode
 	PirlGuardFlag = cli.BoolFlag{
 		Name:  "PirlGuard",
-		Usage: "When enabled, Pirlguard rules will be enforced to control re-org limits. Default pirlgaurd setting is 250 blocks"
+		Usage: "When enabled, Pirlguard rules will be enforced to control re-org limits. Default pirlgaurd setting is 250 blocks",
 	}
 	SyncModeFlag    = TextMarshalerFlag{
 		Name:  "syncmode",
